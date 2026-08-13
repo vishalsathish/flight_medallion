4fc74819345e7de2490c6f34a7596009c0b02ae1
vishalsathish <vishalsathishbusiness@gmail.com>
2026-08-13 00:13:32 -0500

    created docker yaml for postgres database; api key env variable

--- explore_api.py
+++ explore_api.py
@@ -1,8 +1,14 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_API_KEY = os.getenv("VISH_API_KEY")
 
 params = {
-    'access_key': MY_ACCESS_KEY
+    'access_key': MY_API_KEY
 }
 
 url = "https://api.aviationstack.com/v1/flights"
@@ -13,5 +19,5 @@ fetch_data_json = fetch_data.json()
 
 # print(fetch_data_json)
 
-with open("sample_flight.json", "w") as file:
+with open("sample_flight_2.json", "w") as file:
     json.dump(fetch_data_json, file, indent=4)
