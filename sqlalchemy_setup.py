de063d4aff7d28f505d1461336d357d379908b08
vishalsathish <vishalsathishbusiness@gmail.com>
2026-08-21 22:57:38 -0500

    working sqlalchemy engine

--- sqlalchemy_setup.py
+++ sqlalchemy_setup.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 USER = "de_user"
 PASSWORD = "de_password"
 HOST = "postgres"
-PORT = "5432:5432"
+PORT = 5432
 DB_NAME = "aviation_db"
 
-url = "postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+url = "postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:5432/{DB_NAME}"
 
 # engine new connection
 engine = create_engine(url, echo=True)
